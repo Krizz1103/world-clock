efd0d472ebadf33b830c62dab220916610cef848
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-01-30 17:10:34 +0000

    Added javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
 
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDate.innerHTML = losAngelesTime.format("dddd Do MMMM YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+
+  let sydneyDate = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDate.innerHTML = sydneyTime.format("dddd Do MMMM YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
